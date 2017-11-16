601ad44da62730e5c5fe5b8e03c3eafee3e45d19
Sean Walker <swlkr.rbl@gmail.com>
2017-11-16 08:09:23 -0800

    Bump coast. Add :uberjar and :main

--- resources/leiningen/new/coast/project.clj
+++ resources/leiningen/new/coast/project.clj
@@ -3,8 +3,9 @@
   :url "https://example.com/FIXME"
   :min-lein-version "2.6.1"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [coast "0.3.1"]]
+                 [coast "0.4.0"]]
   :repl-options {:init-ns {{name}}.core}
+  :main {{name}}.core
   :plugins [[lein-environ "1.1.0"]]
   :source-paths ["src"]
   :test-paths ["test"]
@@ -18,4 +19,6 @@
             "gen/model"      ["run" "-m" "coast.generators/model" "{{sanitized}}"]
             "gen/controller" ["run" "-m" "coast.generators/controller" "{{sanitized}}"]
             "gen/view"       ["run" "-m" "coast.generators/view" "{{sanitized}}"]
-            "gen/mvc"        ["run" "-m" "coast.generators/mvc" "{{sanitized}}"]})
+            "gen/mvc"        ["run" "-m" "coast.generators/mvc" "{{sanitized}}"]}
+  :profiles {:uberjar {:aot :all
+                       :uberjar-name "{{name}}.jar"}})
