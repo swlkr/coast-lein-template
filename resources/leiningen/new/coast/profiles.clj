d83c50b0d99b663071efc9060d625da352f5b0d3
Sean Walker <swlkr.rbl@gmail.com>
2018-01-18 07:59:38 -0800

    Fix up profiles.clj

--- resources/leiningen/new/coast/profiles.clj
+++ resources/leiningen/new/coast/profiles.clj
@@ -1,26 +1,19 @@
-{:dev  {:env {:db-spec-or-url "postgres://localhost:5432/{{sanitized}}_dev"
-              :admin-db-spec-or-url "postgres://localhost:5432/{{sanitized}}_dev"
-              :secret "{{dev-secret}}"
-              :port "1337"
-              :coast-env "dev"}}
- ;; this profile just show how to config other jdbc databases.
- :sample-jdbc   {:env 
-                    {:db-spec-or-url 
-                        {:classname "com.mysql.jdbc.Driver" ; must be in classpath
-                         :subprotocol "mysql"
-                         :dbname "{{sanitized}}_sample"
-                         :subname "//127.0.0.1:3306/{{sanitized}}_sample?characterEncoding=UTF-8"
-                         :user "root"
-                         :password "helloworld"}}
-                    {:admin-db-spec-or-url 
-                        {:classname "com.mysql.jdbc.Driver" ; must be in classpath
-                         :subprotocol "mysql"
-                         :dbname "{{sanitized}}_sample"
-                         :subname "//127.0.0.1:3306/{{sanitized}}_sample?characterEncoding=UTF-8"
-                         :user "root"
-                         :password "helloworld"}}}
+{:dev {:env {:db-spec-or-url "postgres://localhost:5432/{{sanitized}}_dev"
+             ; use this if your db is password protected
+             ;:admin-db-spec-or-url "postgres://localhost:5432/{{sanitized}}_dev"
+             :secret "{{dev-secret}}"
+             :port "1337"
+             :coast-env "dev"}}
+ ;; this profile just shows how to configure other jdbc databases
+ :sample-jdbc {:env {:db-spec-or-url {:classname "com.mysql.jdbc.Driver" ; must be in classpath
+                                      :subprotocol "mysql"
+                                      :dbname "{{sanitized}}_sample"
+                                      :subname "//127.0.0.1:3306/{{sanitized}}_sample?characterEncoding=UTF-8"
+                                      :user "root"
+                                      :password "helloworld"}}}
  :test {:env {:db-spec-or-url "postgres://localhost:5432/{{sanitized}}_test"
-              :admin-db-spec-or-url "postgres://localhost:5432/{{sanitized}}_test"
+              ; use this if your db is password protected
+              ;:admin-db-spec-or-url "postgres://localhost:5432/{{sanitized}}_test"
               :secret "{{test-secret}}"
               :port "1337"
               :coast-env "test"}}}
