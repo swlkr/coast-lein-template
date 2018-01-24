cf8c7464f9e1e7faefd1c4cd2a42c7494ebef870
Sean Walker <swlkr.rbl@gmail.com>
2018-01-23 22:12:00 -0800

    Bump coast

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject coast/lein-template "0.6.5"
+(defproject coast/lein-template "0.6.6"
   :description "A lein template for coast"
   :url "https://coastonclojure.com"
   :license {:name "MIT License"
